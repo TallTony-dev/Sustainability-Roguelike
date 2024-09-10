6481d60adef3eec6862fbdae92ee2ca8b303ebaa
ThatDogGuy <84847651+ThatDogGuy@users.noreply.github.com>
2024-09-10 17:06:35 -0300

    Update LevelEditor.cs

--- Scripts/LevelEditor.cs
+++ Scripts/LevelEditor.cs
@@ -11,9 +11,37 @@ namespace Monogame_Cross_Platform.Scripts
     public class LevelEditor
     {
         public bool isInEditor = false;
+        public bool isChangingTile;
+        Tile tileToPlace = new Tile();
         public void Update()
         {
-
+var kstate = Keyboard.GetState();
+            (int, int) playerAbsPos = TileMap.PosToAbsTileMapPos(player.position);
+            if (kstate.IsKeyDown(Keys.E))
+            {
+                TileMap.tileMap[playerAbsPos.Item1, playerAbsPos.Item2] = tileToPlace;
+            }
+            if (kstate.IsKeyDown(Keys.Q) && !isChangingTile)
+            {
+                isChangingTile = true;
+            }
+            if (isChangingTile)
+            {
+                if (kstate.IsKeyDown(Keys.Z))
+                {
+                    tileToPlace.textureIndex = 
+                }
+                if (kstate.IsKeyDown(Keys.X))
+                {
+                    tileToPlace.isBarrier = 
+                }
+                if (kstate.IsKeyDown(Keys.C))
+                {
+                    tileToPlace.statusGiven = 
+                }
+                if (kstate.IsKeyDown(Keys.V))
+                {
+                    tileToPlace.breakEffect = 
         }
     }
 }
